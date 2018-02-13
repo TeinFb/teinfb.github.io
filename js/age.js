66f1bd2eadee2ba14ee6308941debaac3af46440
TeinFb <31733944+TeinFb@users.noreply.github.com>
2018-02-13 13:09:44 +0100

    Try age.js
    in progress

--- js/age.js
+++ js/age.js
@@ -1,8 +1,6 @@
-$('.age').function(){
 var birthdate = new Date("1991/03/11");
 var cur = new Date();
 var diff = cur-birthdate; // This is the difference in milliseconds
 var toto = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
 console.log(age);
 $('.age')span.html(Math.round(age)+' ans');
-}
