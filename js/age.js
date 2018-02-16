9274e21f9fa06f4f891bc91c8e2350c13d4510e6
TeinFb <31733944+TeinFb@users.noreply.github.com>
2018-02-16 16:22:45 +0100

    Âge fix
    Finally ! Js âge fix \o/

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 var birthdate = new Date("1991/03/11");
 var cur = new Date();
 var diff = cur-birthdate; // This is the difference in milliseconds
-var toto = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
-console.log(age);
-$('.age')span.html(Math.round(age)+' ans');
+var test = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
+console.log(test);
+$('#age').html(test+' ans');
--- js/progress-bar.js
+++ js/progress-bar.js
@@ -4,6 +4,14 @@ var coursJs = {
 	test2: function() { console.log('lel'); }
 }
 */
+//Calcul âge 
+var birthdate = new Date("1991/03/11");
+var cur = new Date();
+var diff = cur-birthdate; // This is the difference in milliseconds
+var test = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
+console.log(test);
+$('#age').html(test+' ans');
+
 
 $(function() {
   // Reccuperer l'offset top de $('#Skill')
