c49472d16faa43a1abfd13967efdf6768da560b0
TeinFb <31733944+TeinFb@users.noreply.github.com>
2018-02-18 16:27:44 +0100

    Fix Age.js
    $(function(){  ...

--- js/age.js
+++ js/age.js
@@ -1,6 +1,8 @@
+$(function(){
 var birthdate = new Date("1991/03/11");
 var cur = new Date();
 var diff = cur-birthdate; // This is the difference in milliseconds
 var test = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
 console.log(test);
-$('#age').html(test+' ans');
+$('.age').html(test+' ans');
+});
--- js/progress-bar.js
+++ js/progress-bar.js
@@ -4,13 +4,13 @@ var coursJs = {
 	test2: function() { console.log('lel'); }
 }
 */
-//Calcul âge 
-var birthdate = new Date("1991/03/11");
-var cur = new Date();
-var diff = cur-birthdate; // This is the difference in milliseconds
-var test = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
-console.log(test);
-$('#age').html(test+' ans');
+// //Calcul âge
+// var birthdate = new Date("1991/03/11");
+// var cur = new Date();
+// var diff = cur-birthdate; // This is the difference in milliseconds
+// var test = Math.floor(diff/31557600000); // Divide by 1000*60*60*24*365.25
+// console.log(test);
+// $('#age').html(test+' ans');
 
 
 $(function() {
@@ -20,9 +20,9 @@ $(function() {
   var limit = $('#skill_pro').height() - 90;
   $(window).scroll();
 
-  var coursJs = {
-    test: "allo"
-  }
+  // var coursJs = {
+  //   test: "allo"
+  // }
 
   var shouldProgressOnScroll = true;
 
