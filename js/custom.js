249eec2ec313f551f23fb79398a13693c5b9431a
TeinFb <31733944+TeinFb@users.noreply.github.com>
2018-02-22 21:55:12 +0100

    Beautify JS
    Remove separate Js file and merged (?) them to unique file

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,54 @@
+/* ========================================================================
+ * self DOM cheatSheet
+ anchors:
+ about
+ skill_pro
+ xp_pro
+
+   ======================================================================== */
+
+
 $(function() {
+
+  /* ========================================================================
+   * ScrollReveal
+     ======================================================================== */
+  window.sr = ScrollReveal();
+  sr.reveal('.intro', {
+    duration: 1000,
+    origin: 'top'
+  });
+  sr.reveal('.skill_pro', {
+    duration: 500,
+    origin: 'top'
+  });
+  sr.reveal('.skill_pro-btn', {
+    delay: 2000,
+    duration: 2000,
+    origin: 'top'
+  });
+  sr.reveal('.xp_pro', {
+    duration: 1000,
+    origin: 'top'
+  });
+  sr.reveal('.foo', {
+    container: '.modal-body'
+  });
+
+  /* ========================================================================
+   * calcul de l'age
+     ======================================================================== */
+  var birthdate = new Date("1991/03/11");
+  var cur = new Date();
+  var diff = cur - birthdate; // This is the difference in milliseconds
+  var age = Math.floor(diff / 31557600000); // Divide by 1000*60*60*24*365.25
+  // console.log(test);
+  $('.age').html(age + ' ans');
+  // console.log(age); //check age
+
+  /* ========================================================================
+   * Custom show/hide modal
+     ======================================================================== */
   $('.toggle-image').on('click', function() {
     var images = $(this).attr('data-image');
     var arrayImages = images.split(',');
@@ -27,4 +77,133 @@ $(function() {
     $('.modal').fadeOut('fast');
     $('.modal-back').fadeOut('fast');
   }
+  /* ========================================================================
+   * ini fullPage
+     ======================================================================== */
+
+  $(document).ready(function() {
+    $('#fullpage').fullpage({
+      anchors: ['firstPage', 'secondPage', 'thirdPage']
+    });
+
+  });
+  /* ========================================================================
+   * Progress Bar animation
+     ======================================================================== */
+  // Reccuperer l'offset top de $('#Skill')
+  var offsetTop = $('#skill_pro').offset().top - 90;
+  var lauched = false;
+  var limit = $('#skill_pro').height() - 90;
+  $(window).scroll();
+  var shouldProgressOnScroll = true;
+  if ($(document).height() <= limit + $(window).height()) {
+    handleProgress();
+  } else {
+    lauched = false;
+  }
+
+  function handleProgress() {
+    $('progress').each(function() {
+      var max = $(this).attr('data-value');
+      $(this).val(0).animate({
+        value: max
+      }, {
+        duration: 1500,
+        progress: function() {
+          var value = $(this)[0].value;
+          var spanpercent = $(this).parent().find('.percent');
+          spanpercent.html(Math.round(value) + ' %');
+        },
+      });
+    });
+  }
+  /* ========================================================================
+   * modif en temps reel de la valeur "humour" en f de position souris
+     ======================================================================== */
+  var initial = $('#humour').attr('data-value');
+
+  $('.Bonus').on("mousemove", function(event) {
+    var offsetLeft = $(this).offset().left + 15;
+    var posLeft = event.pageX - offsetLeft;
+    var max = $('#humour').width();
+    var newValue = (posLeft / max) * 100;
+
+    // var posLeft = scrollTop() - offsetLeft;
+    // console.log (scrollTop);
+    setVal(newValue);
+  });
+
+  $('.Bonus').on("mouseout", function(e) {
+    setVal(initial);
+  });
+
+  function setVal(val) {
+    var rounded = Math.round(val);
+    if (rounded > 100) {
+      rounded = 100;
+    } else if (rounded < 0) {
+      rounded = 0
+    }
+    $('#humour').attr('value', rounded);
+    $('#percent-bonus').html(Math.round(rounded) + ' %');
+  }
+  /* ========================================================================
+  // modif en temps reel de la valeur "humour" en f de position souris
+  //Mobile
+  /* ======================================================================== */
+  var initial = $('#humour').attr('data-value');
+
+  $('.Bonus').on("touchmove", function(event) {
+    event.preventDefault();
+    var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
+    var offsetTop = $('.Bonus').offset().top;
+    var posLeft = touch.pageY - offsetTop;
+    var max = $(this).height();
+    var test = $('#skill_pro').width('%');
+    var newValue = 100 - (posLeft / max) * 100;
+    // console.log(test2);
+    // console.log(newValue);
+    // console.log(posLeft);
+    setVal(newValue);
+  });
+
+  $('.Bonus').on("touchend", function(e) {
+    setVal(initial);
+  });
+
+  function setVal(val) {
+    var rounded = Math.round(val);
+    if (rounded > 100) {
+      rounded = 100;
+    } else if (rounded < 0) {
+      rounded = 0
+    }
+    $('#humour').attr('value', rounded);
+    $('#percent-bonus').html(Math.round(rounded) + ' %');
+  }
+
+
+
+  /* ========================================================================
+   * Mettre un evenement on scroll sur window pour choper le scroll actuelle
+   ======================================================================== */
+  $(window).on('scroll', function(e) {
+    var windowScrollTop = $(this).scrollTop();
+    // si le window scroll est > a l'offset top de #skill
+    if (windowScrollTop >= offsetTop && !lauched) {
+      lauched = true;
+      handleProgress();
+    }
+  });
+  /* ========================================================================
+   * collapse Navbar on Scroll and Click
+     ======================================================================== */
+  $(document).on('click scroll', function() {
+    $('.collapse').collapse('hide');
+  })
+
+
+
+
+
 });
